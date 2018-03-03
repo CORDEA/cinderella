bb2d572cb71c1f5e30f6a346137d59dcb57ebd2d
Yoshihiro Tanaka <contact@cordea.jp>
2018-03-03 16:12:22 +0900

    update remaining time

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,3 +9,33 @@ $('.timepicker').pickatime({
     ampmclickable: true,
     aftershow: function(){}
 });
+
+function updateTime(current) {
+    const elem = document.getElementById("current");
+    elem.innerText = current;
+}
+
+async function fetchCurrentTime() {
+    try {
+        const response = await fetch("/current");
+        return await response.json();
+    } catch (e) {
+    }
+}
+
+async function wait(ms) {
+    return new Promise(r => setTimeout(r, ms))
+}
+
+async function startRefreshCurrentTime() {
+    const response = await fetchCurrentTime()
+    console.log(response);
+    if (response.is_stopped) {
+        return;
+    }
+    updateTime(response.current)
+    await wait(1000);
+    await startRefreshCurrentTime();
+}
+
+startRefreshCurrentTime();
