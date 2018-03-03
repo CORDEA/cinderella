40996f5d99844b23e7d2a6160b1c5d0fdecb3be8
Yoshihiro Tanaka <contact@cordea.jp>
2018-03-03 13:52:45 +0900

    control time picker

---
+++ public/javascripts/main.js
@@ -0,0 +1,11 @@
+$('.timepicker').pickatime({
+    default: 'now',
+    fromnow: 0,
+    twelvehour: false,
+    donetext: 'OK',
+    cleartext: 'Clear',
+    canceltext: 'Cancel',
+    autoclose: false,
+    ampmclickable: true,
+    aftershow: function(){}
+});
